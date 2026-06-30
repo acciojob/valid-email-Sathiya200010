0f509c5c27e4ad328dacbd2212a3440cde35453a
Sathiya R <rskeerthi01@gmail.com>
2026-06-30 23:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	let pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return pattern.test(email);
+
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt('Enter an email address.");
 alert(validEmail(str));
